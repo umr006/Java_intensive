d9fa0d93eb6e645ab70f28ac30dd95644220ebad
umr006 <107504297+umr006@users.noreply.github.com>
2024-06-07 10:25:40 +0500

    Update Program.java

--- Java_day01/src/ex00/Program.java
+++ Java_day01/src/ex00/Program.java
@@ -2,7 +2,13 @@ package ex00;
 
 public class Program {
     public static void main(String[] args) {
-        User u = new User(1, "Kms", -100);
-        System.out.println(u.getBalance());
+                User user1 = new User(1, "John", 340);
+                User user2 = new User(2, "Drakula", 200);
+                System.out.println(user1);
+                System.out.println(user2);
+                Transaction trans1 = new Transaction(user1, user2, Transaction.TransactionCategory.DEBIT, 300);
+                System.out.println(trans1);
+                System.out.println(user1);
+                System.out.println(user2);
     }
 }
