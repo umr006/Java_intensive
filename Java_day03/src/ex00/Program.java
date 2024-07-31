16e4076776ef64aca54e41c22daf195776b3d7f4
umr006 <umar.ausheff@yandex.ru>
2024-08-01 00:09:41 +0500

    day3

---
+++ Java_day03/src/ex00/Program.java
@@ -0,0 +1,38 @@
+package ex00;
+
+public class Program {
+    public static class threadPrint implements Runnable {
+
+        private int cnt = 0;
+        private String str = "";
+
+        public threadPrint(String str, int cnt) {
+            this.cnt = cnt;
+            this.str = str;
+        }
+
+        public static void print(String str, int cnt) {
+            for (int i = 0; i < cnt; i++) {
+                System.out.println(str);
+            }
+        }
+
+        @Override
+        public void run() {
+            print(str, cnt);
+        }
+    }
+
+    public static void main(String[] args) {
+        int cnt = Integer.parseInt(args[0].substring(8));
+
+        Thread egg = new Thread(new threadPrint("Egg", cnt));
+        Thread hen = new Thread(new threadPrint("Hen", cnt));
+
+        egg.start();
+        hen.start();
+
+        threadPrint.print("Human", cnt);
+
+    }
+}
