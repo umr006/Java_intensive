3ace98f152f800837c73c3ae7fc2752a468e14fb
umr006 <umar.ausheff@yandex.ru>
2024-08-12 00:00:17 +0500

    MavenEx03

---
+++ ImagetoCharEX00/src/main/java/edu/21school/printer/logic/ImageToChar.java
@@ -0,0 +1,46 @@
+package edu.school21.printer.logic;
+
+import com.diogonunes.jcdp.color.ColoredPrinter;
+import com.diogonunes.jcdp.color.api.Ansi;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageToChar {
+    private char black;
+    private char white;
+    private BufferedImage bufferedImage;
+    //private static String PATH_TO_FILE = "/Users/new/Desktop/Java_intensive/Java_day04/src/ex01/ImagesToChar/src/resources/it.bmp";
+    private static String PATH_TO_FILE = "/Users/new/Desktop/Java_intensive/Java_day04/src/ex02/ImageToChar/src/resources/it.bmp";
+    public ImageToChar(char black, char white) {
+        this.black = black;
+        this.white = white;
+    }
+
+    public void printImage() {
+        try {
+            bufferedImage = ImageIO.read(new File(PATH_TO_FILE));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+        ColoredPrinter cp = new ColoredPrinter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Color color = new Color(bufferedImage.getRGB(j, i));
+                if (color.equals(Color.BLACK)) {
+                    cp.print(" ", Ansi.Attribute.NONE, Ansi.FColor.NONE, Ansi.BColor.RED);
+                } else {
+                    cp.print(" ", Ansi.Attribute.NONE, Ansi.FColor.NONE, Ansi.BColor.GREEN);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
